975c6e3c4220c9daac2a79bdbc16207eaad72d01
mass-js <105040888+mass-js@users.noreply.github.com>
2023-04-14 22:15:01 +0800

    Update urlpa.py

--- urlpa.py
+++ urlpa.py
@@ -3,7 +3,7 @@ import re
 
 # 设置headers
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    'User-Agent': '******'}
 # 设置搜索关键词
 keyword = "inurl:/php?id=1"
 
